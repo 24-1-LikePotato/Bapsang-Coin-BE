ff46a46f77d355ade18284400405fa4bac967451
kaswhy <sxshx818@naver.com>
2024-07-23 21:36:49 +0900

    feat: mysql 연결 설정

--- Zipbab/Zipbab/settings.py
+++ Zipbab/Zipbab/settings.py
@@ -85,8 +85,15 @@ WSGI_APPLICATION = 'zipbab.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'zipbab-db',
+        'USER':'master',
+        'PASSWORD':'zipbab0000',
+        'HOST': 'zipbab-mysql.cx2ugw86egw2.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command' : 'SET sql_mode="STRICT_TRANS_TABLES"'
+        }
     }
 }
 
