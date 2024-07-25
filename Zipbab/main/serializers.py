bb58fc2ab3f0fdf0293dcdb6c6a66741c90b2c5c
bmh7190 <bmh7190@naver.com>
2024-07-25 17:57:01 +0900

    feat: #14 fridge_ingredient_post

--- Zipbab/main/serializers.py
+++ Zipbab/main/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Fridge, FridgeIngredient, Ingredient
+from django.utils import timezone
 
 class FridgeIngredientSerializer(serializers.ModelSerializer):
     ingredient_name = serializers.SerializerMethodField()
@@ -34,3 +35,32 @@ class FridgeSerializer(serializers.ModelSerializer):
     def get_ingredients(self, obj):
         fridge_ingredients = FridgeIngredient.objects.filter(fridge=obj)
         return FridgeIngredientSerializer(fridge_ingredients, many=True).data
+
+
+class FridgeIngredientCreateSerializer(serializers.Serializer):
+    ingredient_name = serializers.CharField(max_length=50)
+    expiration_date = serializers.DateField()
+    def create(self, validated_data):
+        ingredient_name = validated_data['ingredient_name']
+        expiration_date = validated_data['expiration_date']
+        
+        # Get or create the ingredient
+        ingredient, created = Ingredient.objects.get_or_create(name=ingredient_name)
+        
+        # Create the FridgeIngredient
+        FridgeIngredient.objects.create(
+            fridge=self.context['fridge'],
+            ingredient=ingredient,
+            expiration_date=expiration_date,
+            purchase_date=timezone.now().date()  # Set the current date as purchase_date
+        )
+        
+        # Return validated_data directly, since we are not returning an instance
+        return validated_data
+
+    def to_representation(self, instance):
+        # Directly use the validated data for representation
+        return {
+            'ingredient_name': instance['ingredient_name'],
+            'expiration_date': instance['expiration_date']
+        }
--- Zipbab/main/views.py
+++ Zipbab/main/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
 from .models import Fridge,FridgeIngredient,User
-from .serializers import FridgeSerializer
+from .serializers import FridgeSerializer,FridgeIngredientCreateSerializer
 
 class FridgeDetailView(APIView):
     def get(self, request, user_id):
@@ -25,5 +25,19 @@ class FridgeDetailView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
     
     
-
+    def post(self, request, user_id):
         
+        # 등록된 유저가 없다면?
+        try:
+            user = User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return Response({'message': '등록된 유저가 없습니다.'}, status=status.HTTP_404_NOT_FOUND)
+        
+        fridge = get_object_or_404(Fridge, user=user)
+
+        serializer = FridgeIngredientCreateSerializer(data=request.data, context={'fridge': fridge})
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': '정상적으로 식재료가 등록되었습니다.'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
