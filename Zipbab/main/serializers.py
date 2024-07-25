cb80136f87d67961ad6aa638afa61071f77972b6
bmh7190 <bmh7190@naver.com>
2024-07-25 20:15:47 +0900

    feat: #14 delete diffrent api

--- Zipbab/main/serializers.py
+++ Zipbab/main/serializers.py
@@ -2,41 +2,6 @@ from rest_framework import serializers
 from .models import Fridge, FridgeIngredient, Ingredient
 from django.utils import timezone
 
-class FridgeIngredientSerializer(serializers.ModelSerializer):
-    ingredient_name = serializers.SerializerMethodField()
-    ingredient_pk = serializers.SerializerMethodField()
-    days_until_expiration = serializers.SerializerMethodField()
-    is_expiring_soon = serializers.SerializerMethodField()
-
-    class Meta:
-        model = FridgeIngredient
-        fields = ['ingredient_name','ingredient_pk', 'days_until_expiration', 'is_expiring_soon']
-
-    def get_ingredient_name(self, obj):
-        return obj.ingredient.name
-    
-    def get_ingredient_pk(self,obj):
-        return obj.ingredient.pk
-
-    def get_days_until_expiration(self, obj):
-        return obj.days_until_expiration()
-
-    def get_is_expiring_soon(self, obj):
-        return obj.is_expiring_soon()
-    
-    
-class FridgeSerializer(serializers.ModelSerializer):
-    ingredients = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Fridge
-        fields = ['user', 'ingredients']
-
-    def get_ingredients(self, obj):
-        fridge_ingredients = FridgeIngredient.objects.filter(fridge=obj)
-        return FridgeIngredientSerializer(fridge_ingredients, many=True).data
-
-
 class FridgeIngredientCreateSerializer(serializers.Serializer):
     ingredient_name = serializers.CharField(max_length=50)
     expiration_date = serializers.DateField()
--- Zipbab/main/views.py
+++ Zipbab/main/views.py
@@ -3,28 +3,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
 from .models import Fridge,FridgeIngredient,User
-from .serializers import FridgeSerializer,FridgeIngredientCreateSerializer
+from .serializers import FridgeIngredientCreateSerializer
 
 class FridgeDetailView(APIView):
-    def get(self, request, user_id):
-
-        # 등록된 유저가 없다면?
-        try:
-            user = User.objects.get(pk=user_id)
-        except User.DoesNotExist:
-            return Response({'message': '등록된 유저가 없습니다.'}, status=status.HTTP_404_NOT_FOUND)
-        
-        fridge = get_object_or_404(Fridge, user_id=user_id)
-        fridge_ingredients = FridgeIngredient.objects.filter(fridge=fridge)
-
-        if not fridge_ingredients.exists():
-            return Response({'message': '등록한 식재료가 없습니다.'}, status=status.HTTP_404_NOT_FOUND)
-        
-        serializer = FridgeSerializer(fridge)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    
     def post(self, request, user_id):
         
         # 등록된 유저가 없다면?
