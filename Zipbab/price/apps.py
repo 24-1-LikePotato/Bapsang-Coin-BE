301c8421df3d35d56ab7c75012431ad27fc9a4f0
kaswhy <sxshx818@naver.com>
2024-07-26 22:22:33 +0900

    fix: migrations 추적제외

--- Zipbab/price/apps.py
+++ Zipbab/price/apps.py
@@ -5,11 +5,4 @@ class PriceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'price'
 
-    def ready(self):
-        from .utils import load_ingredients_from_csv
-        from .models import Ingredient
-        from django.conf import settings
 
-        if not Ingredient.objects.exists():
-            file_path = settings.CSV_FILE_PATH
-            load_ingredients_from_csv(file_path)
