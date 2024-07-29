083dd23d9a0db9c0e92675db4632f78a7cd7c50c
chanbro0524 <12202049@inha.edu>
2024-07-29 14:57:43 +0900

    fix:식재료 기본페이지 url수정

--- Zipbab/main/urls.py
+++ Zipbab/main/urls.py
@@ -4,8 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ingredient/', views.ingredientpage, name='ingredient-page'),
-    path('search/', views.ingredientsearch, name='ingredient-search'),
-    path('related_recipe/', views.related_recipe, name='related-recipe'),
-    path('recipe/search/', views.recipesearch, name='recipe-search'),
+    path('ingredient/', views.ingredientpage, name='ingredient-page')
 ]
