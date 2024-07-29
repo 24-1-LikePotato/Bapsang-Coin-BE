8a36eb5543e41117fc161715329ed35b507a2c6f
kaswhy <sxshx818@naver.com>
2024-07-29 17:15:07 +0900

    feat: 오늘의 식재료 기능 구현

--- Zipbab/price/urls.py
+++ Zipbab/price/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('ingredient-price', views.UpdateIngredientPriceView.as_view(), name='update-ingredient-price'),
+    path('today-price', views.TodayPriceView.as_view(), name='price-range'),
 ]
