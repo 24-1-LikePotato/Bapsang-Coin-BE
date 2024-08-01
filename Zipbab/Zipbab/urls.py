6fd137c75ba5626894767349595bf20ddd003f30
kaswhy <sxshx818@naver.com>
2024-08-02 00:09:18 +0900

    fix: static 관련 url 수정

--- Zipbab/Zipbab/urls.py
+++ Zipbab/Zipbab/urls.py
@@ -28,5 +28,4 @@ urlpatterns = [
     # Swagger url
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
