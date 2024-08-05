51a91ae84369ad7a4080912de8e2a926795455f3
kaswhy <sxshx818@naver.com>
2024-08-05 21:42:49 +0900

    fix: uwsgi 종료 코드 로직 수정
    1. 코드디플로이에 의해 실행되는 프로세스 우선 종료
    2. 직접 실행한 uwsgi 종료

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
-# uWSGI 프로세스 ID를 찾기
-uwsgi_pids=$(ps -ef | grep '[u]wsgi' | grep -- '--ini uwsgi.ini' | awk '{print $2}')
+# uWSGI 프로세스 ID를 찾기 (전체 경로 포함)
+uwsgi_pids=$(ps -ef | grep '[/]home/ubuntu/Zipbab-Coin-BE/Zipbab/venv/bin/uwsgi --ini uwsgi.ini' | awk '{print $2}')
 
 # 프로세스 ID가 있으면 종료
 if [ -n "$uwsgi_pids" ]; then
     echo "Stopping uWSGI processes with PIDs: $uwsgi_pids"
-    kill $uwsgi_pids
+    kill -9 $uwsgi_pids
     echo "uWSGI processes stopped."
 else
     echo "No uWSGI processes found."
+fi
+
+# 추가적으로 남아있는 uWSGI 프로세스 확인 및 종료
+remaining_pids=$(pgrep -f "uwsgi --ini uwsgi.ini")
+if [ -n "$remaining_pids" ]; then
+    echo "Found remaining uWSGI processes. Stopping them with PIDs: $remaining_pids"
+    kill -9 $remaining_pids
+    echo "Remaining uWSGI processes stopped."
+fi
+
+# 프로세스가 완전히 종료되었는지 확인
+sleep 2
+if pgrep -f "uwsgi --ini uwsgi.ini" > /dev/null; then
+    echo "Warning: Some uWSGI processes may still be running."
+else
+    echo "All uWSGI processes have been successfully terminated."
 fi
