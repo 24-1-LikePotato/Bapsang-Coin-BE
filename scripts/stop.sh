d47752288edc040dd81bbd5c3e399e51d96c8fd4
kaswhy <sxshx818@naver.com>
2024-08-06 00:18:50 +0900

    fix: 다시 이걸로

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# uWSGI 프로세스 ID를 찾기 (전체 경로 포함)
-uwsgi_pids=$(ps -ef | grep '[/]home/ubuntu/Zipbab-Coin-BE/Zipbab/venv/bin/uwsgi --ini uwsgi.ini' | awk '{print $2}')
+# uWSGI 프로세스 ID를 찾기
+uwsgi_pids=$(ps -ef | grep '[u]wsgi' | grep -- '--ini uwsgi.ini' | awk '{print $2}')
 
 # 프로세스 ID가 있으면 종료
 if [ -n "$uwsgi_pids" ]; then
     echo "Stopping uWSGI processes with PIDs: $uwsgi_pids"
-    kill -9 $uwsgi_pids
+    kill $uwsgi_pids
     echo "uWSGI processes stopped."
 else
     echo "No uWSGI processes found."
