86f23f78c06c85583667a8feb18000dfaecf5fd5
kaswhy <sxshx818@naver.com>
2024-08-05 20:44:45 +0900

    fix: 레시피 저장 url 수정

--- Zipbab/main/urls.py
+++ Zipbab/main/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('ingredient', MonthStoreView.as_view() , name='ingredient-page'),
     path('fridge/<int:user_id>', FridgeDetailView.as_view(), name='fridge-detail'),
     path('recipe/update-all-ingredients', RecipeIngredientStoreView.as_view(), name='update-all-recipe-ingredients'),
-    path('recipe',RecipeStoreView.as_view()),
+    path('recipe/store',RecipeStoreView.as_view()),
     path('today-recipe', TodayRecipeView.as_view()),
     path('recipe/update', views.RecipeUpdateView.as_view(), name='recipe-update'),
 ]
