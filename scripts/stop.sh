91a21c7b280c1465174a905f172db9a7156520a2
kaswhy <sxshx818@naver.com>
2024-08-06 00:29:24 +0900

    fix: 종료 코드 재수정
    uwsgi -- uwsgi.ini 종료 포함

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -11,3 +11,11 @@ if [ -n "$uwsgi_pids" ]; then
 else
     echo "No uWSGI processes found."
 fi
+
+# 추가적으로 남아있는 uWSGI 프로세스 확인 및 종료
+remaining_pids=$(pgrep -f "uwsgi --ini uwsgi.ini")
+if [ -n "$remaining_pids" ]; then
+    echo "Found remaining uWSGI processes. Stopping them with PIDs: $remaining_pids"
+    kill -9 $remaining_pids
+    echo "Remaining uWSGI processes stopped."
+fi
